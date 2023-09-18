f311e95a1ad832889ee1d9933190ea6b61aeb55d
Damian <naimad98@gmail.com>
2023-09-18 17:38:14 +1000

    added main pages

---
+++ tailwind.config.cjs
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	daisyui: {
+		themes: [
+		  {
+			mytheme: {
+			
+   "primary": "#5eead4",
+			
+   "secondary": "#93c5fd",
+			
+   "accent": "#c084fc",
+			
+   "neutral": "#2a323c",
+			
+   "base-100": "#1d232a",
+			
+   "info": "#3abff8",
+			
+   "success": "#36d399",
+			
+   "warning": "#fbbd23",
+			
+   "error": "#f87272",
+			},
+		  },
+		],
+	  },
+	plugins: [require("daisyui")],
+}
